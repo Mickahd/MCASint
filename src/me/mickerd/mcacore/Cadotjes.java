b7d5198642aef03b66d49929f54b6210aca71d8e
MICHEL <eftelaarmedia>
2015-12-04 14:30:48 +0100

    V1

---
+++ src/me/mickerd/mcacore/Cadotjes.java
@@ -0,0 +1,16 @@
+package me.mickerd.mcacore;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Cadotjes implements Listener {
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e){
+		Player p = e.getPlayer();
+		p.setScoreboard(scoreClass.scoreboard);
+		
+	}
+}
