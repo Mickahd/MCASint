dd177e12e260b01524b488921821cc14e4fe5b29
MICHEL <eftelaarmedia>
2015-12-04 14:20:13 +0100

    xd

--- src/me/mickerd/mcacore/main.java
+++ src/me/mickerd/mcacore/main.java
@@ -1,7 +1,19 @@
 package me.mickerd.mcacore;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
+	
+	static Plugin pl = null;
+	public void onEnable(){
+		pl = this;
+	}
+	public void onDisable(){
+		pl = null;
+	}
+	public Plugin getPl(){
+		return pl;
+	}
 
 }
